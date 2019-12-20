ac9ca6fb8c577fac80ce84769dd9500f78b584d2
Mayank Saboo <mayanksaboonsk@gmail.com>
2019-12-20 23:25:52 +0530

    add sidebar to notebook and lab

--- jovian/__init__.py
+++ jovian/__init__.py
@@ -408,3 +408,31 @@ def notify(data, verbose=True, safe=False):
             log('message_sent:' + str(res.get('data').get('messageSent')))
         else:
             log(str(res.get('errors')[0].get('message')), error=True)
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+def load_jupyter_server_extension(nb_server_app):
+    """
+    Called when the extension is loaded.
+
+    Args:
+        nb_server_app (NotebookWebApplication): handle to the Notebook webserver instance.
+    """
+    pass
+
+
+# def _jupyter_server_extension_paths():
+#     return [{
+#         'module': 'jupyter_sidebar'
+#     }]
+
+
+def _jupyter_nbextension_paths():
+    return [{
+        'section': 'notebook',
+        'src': 'static',
+        'dest': 'jupyter_sidebar',
+        'require': 'jovian_nb_ext/default'
+    }]
